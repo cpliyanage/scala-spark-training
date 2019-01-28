a22163d4fb3356306e43d7e7447deb7dffdabb4e
AkashJay <akash.jayaweera@kohls.com>
2019-01-28 08:48:49 +0530

    (Spark Excersise 1.1) Divide the functionality of the program to seperate methods

--- src/main/scala/SparkExcercise1_1.scala
+++ src/main/scala/SparkExcercise1_1.scala
@@ -1,5 +1,8 @@
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.io.BufferedSource
 
 /**
   * Created by akash on 1/24/19.
@@ -8,13 +11,15 @@ object SparkExcercise1_1 {
 
   def main(args: Array[String]) {
     Logger.getLogger("org").setLevel(Level.ERROR)
-    val conf = new SparkConf().setAppName("avgHousePrice").setMaster("local[3]")
+    val conf = new SparkConf().setAppName("SparkClickCount").setMaster("local[3]")
     val sc = new SparkContext(conf)
 
-    val lines = sc.textFile("in/clickstream.csv")
+    val lines: RDD[String] = sc.textFile("in/clickstream.csv")
 
-    val Rdd = lines.map(line => (line.split(",")(2)))
-//    val Rdd = lines.map(line => (line.split(",")(2)) +" "+ (line.split(",")(3)))
+
+    val listArr = ScalaExcercise1.getChoiseOfGrouping(List("user", "category"))
+
+    val Rdd = getLinesToListArr(lines,listArr)
 
     val PairRdd = Rdd.map(word => (word, 1))
 
@@ -22,11 +27,26 @@ object SparkExcercise1_1 {
     val swapKeyValue = Count.map(c => (c._2 , c._1))
     val sortedRDD = swapKeyValue.sortByKey(ascending = false)
 
-    val DescOrderPairRDD = sortedRDD.map( a => (a._2,a._1))
+    val DescOrderPairRDD: RDD[(String, Int)] = sortedRDD.map(a => (a._2,a._1))
 
-    for ((word, count) <- DescOrderPairRDD.collect()) println(word + " : " + count)
+    printResult(DescOrderPairRDD)
 
 
   }
 
+  def printResult (PairRDD: RDD[(String, Int)]) = {
+
+    for ((word, count) <- PairRDD.collect()) println(word + " : " + count)
+  }
+
+  def getLinesToListArr (lines: RDD[String], list: List[Int]) : RDD[String]  = {
+
+    val listLen : Int = list.length
+      listLen match {
+        case 1  => return lines.map(line => (line.split(",")(list(0))))
+        case 2  => return lines.map(line => (line.split(",")(list(0)) + "," + (line.split(",")(list(1)))))
+        case 3  => return lines.map(line => (line.split(",")(list(0)) + "," + (line.split(",")(list(1)))+ "," + (line.split(",")(list(2)))))
+        case 4  => return lines.map(line => (line.split(",")(list(0)) + "," + (line.split(",")(list(1)))+ "," + (line.split(",")(list(2)))+ "," + (line.split(",")(list(3)))))
+      }
+  }
 }
